2fc13bd9c616a1756d917a9435c7a434ab210a3a
Bea Graboloza <beagraboloza@gmail.com>
2021-06-08 17:40:50 -0400

    proyecto inicial

--- ChessEngine/ChessEngine.cpp
+++ ChessEngine/ChessEngine.cpp
@@ -1,4 +1,4 @@
-// Chess Engine.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// ChessEngine.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
 #include <iostream>

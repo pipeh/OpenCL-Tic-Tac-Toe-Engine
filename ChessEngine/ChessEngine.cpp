c5433e1d1ec339a881cf233c05da8c939cfcca45
Bea Graboloza <beagraboloza@gmail.com>
2021-06-16 12:55:31 -0400

    añadidas 3 ideas para enviar arboles a gpu

--- ChessEngine/ChessEngine.cpp
+++ ChessEngine/ChessEngine.cpp
@@ -22,7 +22,205 @@ int branchCalculationCPU() {
 	return 0;
 }
 
+//Tratando de crear una arbol con arrays
+
+// Idea 1
+
+	//				  [4]
+	//				/  |  \
+	//			[2]   [3]  [5]
+	//		    / \	   |	|
+	// 		  [1] [6] [7]  [0]
+
+	// count = 0
+
+	// agregar root
+	// [ [4, [] ] ] count = 1
+	
+	// agregar hijos
+	// [ [4, [1] ], [2, [] ] ] count = 2
+	// [ [4, [1, 2] ], [2, [] ], [3, [] ] ] count = 3
+	// [ [4, [1, 2, 3] ], [2, [] ], [3, [] ], [5, [] ] ] count = 4
+
+	// agregarle los hijos a cada hijo
+
+	// hijo 1
+	// [ [4, [1, 2, 3] ], [2, [4] ], [3, [] ], [5, [] ], [1, [] ] ] count = 5
+	// [ [4, [1, 2, 3] ], [2, [4, 5] ], [3, [] ], [5, [] ], [1, [] ], [6, [] ] ] count = 6
+
+	// hijo 2
+	// [ [4, [1, 2, 3] ], [2, [4, 5] ], [3, [6] ], [5, [] ], [1, [] ], [6, [] ], [7, [] ] ] count = 7
+
+	// hijo 3
+	// [ [4, [1, 2, 3] ], [2, [4, 5] ], [3, [6] ], [5, [7] ], [1, [] ], [6, [] ], [7, [] ], [0, [] ] ] count = 8
+
+	//agregarle los hijos a cada hjo
+	// [ [4, [1, 2, 3] ], [2, [4, 5] ], [3, [6] ], [5, [7] ], [1, Null ], [6, [] ], [7, [] ], [0, [] ] ] count = 8
+	// [ [4, [1, 2, 3] ], [2, [4, 5] ], [3, [6] ], [5, [7] ], [1, Null ], [6, Null ], [7, [] ], [0, [] ] ] count = 8
+	// [ [4, [1, 2, 3] ], [2, [4, 5] ], [3, [6] ], [5, [7] ], [1, Null ], [6, Null ], [7, Null ], [0, [] ] ] count = 8
+	// [ [4, [1, 2, 3] ], [2, [4, 5] ], [3, [6] ], [5, [7] ], [1, Null ], [6, Null ], [7, Null ], [0, Null ] ] count = 8
+
+	// Para accedeer a los hijos se revisa en el nodo que posicion dice que tiene el hijo y arraytree[pos]
+
+	//fin :)
+
+	int* TreeToList(int* tree){
+		int* arraytree = [];											// initial arraytree
+		int count = 0;
+		count = AddNode(count, tree, arraytree);  						// add root
+ 		AddChilds(count, tree, *arraytree); 							// add root childs
+		return arraytree;
+	}
+
+	int* AddChilds(int count, int* tree, int* arraytree){
+		if(tree.childs == Null){										// if node doest have childs 
+			*arraytree[1] = Null; 										// the child positions in the node are null
+			return;
+		}
+		for(int i = 0; i < tree.childs.len(); i++){ 					// for each child
+			*arraytree[1] += [count]; 									// save the position where are you going to put it
+			count = AddNode(int count, int* tree, int* arraytree); 		// add the node, increasing de counting for the next one
+		}
+		for(int i = 0; i < tree.childs.len(); i++){						// for each already added child
+			AddChilds(int count, int* tree, int* arraytree);			// add its childs
+		}
+	}
+
+	int AddNode(int count, int* tree, int* arraytree){
+		*arraytree = [tree.value, []];									// add the node with empty child positions
+		count++;														// increase the counter
+		arraytree++;													// move to the next position
+		return count;													// return the new position
+	}
+
+// Idea 2
+
+	//				  [4]
+	//				/  |  \
+	//			[2]   [3]  [5]
+	//		    / \	   |	|
+	// 		  [1] [6] [7]  [0]
+
+	// count = 0
+
+	// agregar root, tiene 3 hijos
+	// [(4,3)] count = 1
+
+	// agregar hijos 
+	// [(4,3), (2,2), (3,1), (5,1)]  count = 4
+
+	// hijo 1, tiene dos hijos, sus indices serán count +1 y count +2
+	// [(4,3), (2,2), (3,1), (5,1), (1,0), (6,0)]  count = 6
+
+	// hijo 2
+	// [(4,3), (2,2), (3,1), (5,1), (1,0), (6,0), (7,0)]  count = 7
+
+	// hijo 3
+	// [(4,3), (2,2), (3,1), (5,1), (1,0), (6,0), (7,0), (0,0)]  count = 8
+
+	// Para acceder en este caso hay que ir recorriendo la lista y sumando cuantos hijos hay 
+
+	// [(4,3), (2,2), (3,1), (5,1), (1,0), (6,0), (7,0), (0,0)]  count = 0
+	// quiero acceder al primer hijo de la raiz count += 1
+	// accedo a arraytree[count]
+	// ahora quiero ir a el segundo hijo de este nodo, cuantos hijos tiene -> empiezo count del principio 
+	// count = 0
+	// count += 3 -> para pasar a los hijos de los hijos 
+	// count += 2 -> segundo hijo del primer hijo 
+	// arraytree[count] es la posicion del hijo 
+
+
+	int* TreeToList(int* tree){
+		int* arraytree = [];											// initial arraytree
+		AddNode(tree, arraytree);  										// add root
+ 		AddChilds(tree, *arraytree); 									// add root childs
+		return arraytree;
+	}
+
+	int* AddChilds(int* tree, int* arraytree){
+		if(tree == Null){												// the previous one was a leaf
+			return;
+		}
+		for(int i = 0; i < tree.childs.len(); i++){ 					// for each child
+			AddNode(int* tree, int* arraytree); 						// add the node
+		}
+		for(int i = 0; i < tree.childs.len(); i++){						// for each already added child
+			AddChilds(int* tree, int* arraytree);						// add its childs
+		}
+	}
+
+	int AddNode(int* tree, int* arraytree){
+		*arraytree = (tree.value, tree.childs.len());					// add the node the amount of childs
+		arraytree++;													// move to the next position
+		return;											
+	}
+
+// Idea 3
+
+	//				  [4]
+	//				/  |  \
+	//			[2]   [3]  [5]
+	//		    / \	   |	|
+	// 		  [1] [6] [7]  [0]
+
+	// count = 0
+
+	// agregar root
+	// [(4,Null)] count = 1
+	
+	// agregar hijos
+	// [ (4, 1), (2, Null) ] count = 2
+	// [ (4, 1), (2, Null), (3, Null) ] count = 3
+	// [ (4, 1), (2, Null), (3, Null), (5, Null) ] count = 4
+
+	// agregarle los hijos a cada hijo
+
+	// hijo 1
+	// [ (4, 1), (2, 4), (3, Null), (5, Null), (1, Null) ] count = 5
+	// [ (4, 1), (2, 4), (3, Null), (5, Null), (1, Null), (6, Null) ] count = 6
+
+	// hijo 2
+	// [ (4, 1), (2, 4), (3, 6), (5, Null), (1, Null), (6, Null), (7, Null) ] count = 7
+
+	// hijo 3
+	// [ (4, 1), (2, 4), (3, 6), (5, 7), (1, Null), (6, Null), (7, Null), (0, Null) ] count = 8
+
+	// Para accedeer a los hijos se revisa en el nodo que posicion dice que tiene el primer hijo y arraytree[pos]
+	// Para segudo hijo será pos +1 y así
+
+	int* TreeToList(int* tree){
+		int* arraytree = [];											// initial arraytree
+		int count = 0;
+		count = AddNode(count, tree, arraytree);  						// add root
+ 		AddChilds(count, tree, *arraytree); 							// add root childs
+		return arraytree;
+	}
+
+	int* AddChilds(int count, int* tree, int* arraytree){
+		if(tree.childs == Null){										// if node doest have childs it ends
+			return;
+		}
+		*arraytree[1] = count; 											// if have childs, save the position where are you going to put the first one
+
+		for(int i = 0; i < tree.childs.len(); i++){ 					// for each child
+			count = AddNode(int count, int* tree, int* arraytree); 		// add the node, increasing de counting for the next one
+		}
+		for(int i = 0; i < tree.childs.len(); i++){						// for each already added child
+			AddChilds(int count, int* tree, int* arraytree);			// add its childs
+		}
+	}
+
+	int AddNode(int count, int* tree, int* arraytree){
+		*arraytree = [tree.value, Null];								// add the node with empty child positions
+		count++;														// increase the counter
+		arraytree++;													// move to the next position
+		return count;													// return the new position
+	}
+
+	//Fin de tratando 
+
 int gameTreeMax(int* P0) {
+
 	// Setting P0 as the root of the game tree T
 	int* T = P0;
 	int M_best = 0;
